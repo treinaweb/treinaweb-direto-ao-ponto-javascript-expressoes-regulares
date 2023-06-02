1b68dc4da3b9d0f524c5e09eaf78d18d7c33667d
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-06-02 10:03:21 -0300

    01:11 - Substituição de string

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const valor = 'O telefone é (11) 9999-9999';
+const data = "2023-05-22";
 
-const regex = /\((\d+)\) (\d+)-(\d+)/;
+const novaData = data.replace(/(\d{4})-(\d{2})-(\d{2})/, (match, ano, mes, dia, offset) => {
+    console.log(match, ano, mes, dia, offset);
+    return `${dia}/${mes}/${ano}`;
+});
 
-const match = valor.match(regex);
-const numeroDoTelefone = match[0].replace(/\D/g, '');
-
-console.log('numero completo: ', match[0]);
-console.log('DDD: ', match[1]);
-console.log(numeroDoTelefone);
+console.log(novaData);
