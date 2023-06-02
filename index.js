904744cf7ec592c4912956060c6d00cbd66a4f86
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-06-02 10:01:39 -0300

    01:07 - Usando o método split()

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const frase = "Os gatos são fofos. Eu amo gatos!";
+const frase = '1, 2, 3; 4.5|6 7';
 
-const regex = /(gatos)/g;
-
-const novaFrase = frase.replace(regex, (match, g1, offset, valor) => {
-    console.log(`Match: ${match} na posição ${offset}: ${g1}`);
-    console.log(`Valor original: ${valor}`);
-    return 'cachorros';
-});
-
-console.log(novaFrase);
+console.log(frase.split(/[\s,;.|]+/));
