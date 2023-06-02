5f58110ec3141abcdb908b743036f24ea72e61a1
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-06-02 10:00:22 -0300

    01:04 - Usando o método exec()

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const frase = "Os cachorro são muito fofos! gatos";
+const regex = /abc/;
 
-const regex = /gatos/;
+// console.log(regex.exec('abcd'));
 
 
-console.log(regex.test(frase));
+const frase = 'A data de hoje é 22/05/2023';
+
+const regex2 = /(\d{2})\/(\d{2})\/(\d{4})/;
+
+const resultado = regex2.exec(frase);
+
+if (resultado !== null) {
+    console.log('Correspondência completa: ', resultado[0]);
+    console.log('Primeiro grupo de captura (Dia): ', resultado[1]);
+    console.log('Segundo grupo de captura (Mês): ', resultado[2]);
+    console.log('Terceiro grupo de captura (Ano): ', resultado[3]);
+    console.log('Índice da correspondência na string: ', resultado.index);
+    console.log('String original: ', resultado.input);
+}
